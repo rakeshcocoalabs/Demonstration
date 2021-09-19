53a0e2475e927da241988d676cd4e31eb0a3526f
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-09-19 22:07:47 +0530

    c

--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': "rkdb",
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432 ',
     }
 }
 
